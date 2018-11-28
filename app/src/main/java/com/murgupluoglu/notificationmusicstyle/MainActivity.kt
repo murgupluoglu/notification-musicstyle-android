ee3f41d733923778067c8d3639b6f0307e8aa4cc
Mustafa Urgupluoglu <mustafa.urgupluoglu@markakod.com>
2018-11-28 17:23:13 +0300

    init

---
+++ app/src/main/java/com/murgupluoglu/notificationmusicstyle/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.murgupluoglu.notificationmusicstyle
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var notificationGenerator: NotificationGenerator = NotificationGenerator()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sendNotificationButton.setOnClickListener {
+            val serviceIntent = Intent(applicationContext, NotificationService::class.java)
+            serviceIntent.setAction(STARTFOREGROUND_ACTION)
+            startService(serviceIntent)
+            notificationGenerator.showBigContentMusicPlayer(applicationContext)
+            Toast.makeText(this, "Sent a big content notification with action buttons.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
